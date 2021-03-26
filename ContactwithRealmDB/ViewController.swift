e50d0c0aab5a60a92b1bae63a54e719aaca1d214
Thành Long <long.nguyen@savvycomsoftware.com>
2021-03-26 14:15:13 +0700

    Initial Commit

---
+++ ContactwithRealmDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactwithRealmDB
+//
+//  Created by savvycom2021 on 3/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
